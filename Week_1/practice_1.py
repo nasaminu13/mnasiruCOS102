6d3a417e46b748fe901cbf2a5dda3c30c2197a04
Muhammad Nasiru <nasaminu13@gmail.com>
2025-03-27 11:19:37 +0100

    Practice 1

---
+++ Week_1/practice_1.py
@@ -0,0 +1,14 @@
+# Integer number
+num = 100
+print(num)
+print("Data type of variable num is", type(num))
+
+# float number
+fnum = 34.35
+print(fnum)
+print("Data type of variable fnum is", type(fnum))
+
+# complex number
+cnum = 3 + 4j
+print(cnum)
+print("Data type variable of cnum is", type(cnum))
